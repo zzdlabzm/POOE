14fa9381ffe3a9d7b3e5935780574c97cf748455
zzdlabzm <125731093+zzdlabzm@users.noreply.github.com>
2023-09-09 16:15:14 +0800

    Delete code directory

--- code/ESM/temp.sh
+++
@@ -1,8 +0,0 @@
-mkdir ./tmp_esm_out/
-#python extract.py esm1b_t33_650M_UR50S input.fasta ./tmp_esm_out/ --include mean --repr_layers 33 --truncation_seq_length 2000
-python generate_ESM.py  esm1b_t33_650M_UR50S  ../../data/training_data/positivedata549.fasta ./output_out/ \
-    --include mean  \
-    --repr_layers 33  \
-    --truncation_seq_length 4000  \
-    --save_file  \
-    ./positivedata549_esm.pkl
--- code/ProtTrans/temp.sh
+++
@@ -1,1 +0,0 @@
-python gen_prottrans.py ../../data/training_data/positivedata549.fasta ./positivedata549_prottrans.pkl
