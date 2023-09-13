d6d260d9bacaef6a5f43e358882c460940d45a78
zzdlabzm <125731093+zzdlabzm@users.noreply.github.com>
2023-09-13 22:48:46 +0800

    Update generate_doc2vec.py

--- features/generate_doc2vec.py
+++ features/generate_doc2vec.py
@@ -57,7 +57,7 @@ def get_documents(seq_list, seq_ids, k, extract_method):
 # read corpus
 seqs = dict()
 count_for_test=1000
-for i in SeqIO.parse("./doc2vec/swissprot_30-5000_50_pos+neg.fasta","fasta"):
+for i in SeqIO.parse("./doc2vec/swissprot_30-5000_50_pos+neg.fasta","fasta"):#load your corpus
     if "|" in i.id:
         k=i.id.split("|")[1]
         if ":" in k:
@@ -81,27 +81,7 @@ for i in SeqIO.parse("../data/training_data/all_pos_and_neg.fasta","fasta"):
     v = str(i.seq)
     seqs_train[k]=v
 
-#print("check",seqs_train.keys()-seqs.keys())
-#def loadfile(name):
-#	f = open(name,'r')
-#	data = [line.strip() for line in f.readlines()]
-#	f.close()
-#	return data
-#data = loadfile('./corpus.fasta')###load corpus
-#seq = {}
-#for i in data:
-#	if i[0]=='>':
-#		name = i
-#	else:
-#		seq[name] = i
-#dic1 = seq
-## print(dic1)
-#sequences = []
-#ids = []
-#for k,v in dic1.items():
-#    sequences.append(v)
-#    ids.append(k)
-#
+
 def doc2vector(seqs, k_len, extract_method, vector_size, window, epoch):
     """
     :param seq_list:
@@ -136,56 +116,7 @@ Feature,name,M = doc2vector(seqs,3,3,32,6,70)
 import pickle 
 with open("./doc2vec/doc2vec.pkl","wb") as f:
     pickle.dump(Feature,f)
-###read positivedata and negativedata
-#from gensim.models.doc2vec import Doc2Vec
-#
-#
-#
-##def loadfile(name):
-##    f = open(name,'r')
-##    data = [line.strip() for line in f.readlines()]
-##    f.close()
-##    return data
-##pos_data = loadfile('./../data/training_data/positivedata549.fasta')
-##neg_data1670 = loadfile('./../data/training_data/negativedata1670.fasta')
-#
-#k = 3
-#vector_size = 32
-#model = Doc2Vec.load('./'+str(k)+'_'+str(vector_size)+'doc2vec.model')
-#
-#pos_seq = {}
-#for i in pos_data:
-#    if i[0]=='>':
-#        name = i
-#    else:
-#        pos_seq[name] = i
-#
-#f = open('./'+str(k)+'_'+str(vector_size)+'pos549_vector.txt','w')
-#for seq_id in pos_seq.keys():
-#    feature_matrix = []
-#    feature_matrix.append([np.mean([model.dv[seq_id + '_%s' % j][a] for j in range(k)]) for a in range(vector_size)])
-#    f.write(str(feature_matrix)+'\n')
-#f.close()
-#
-#
-#neg_seq1670 = {}
-#for i in neg_data1670:
-#    if i[0]=='>':
-#        name = i
-#    else:
-#        neg_seq1670[name] = i
-#
-#f = open('./'+str(k)+'_'+str(vector_size)+'neg1670_vector.txt','w')
-#for seq_id in neg_seq1670.keys():
-#    feature_matrix = []
-#    feature_matrix.append([np.mean([model.dv[seq_id + '_%s' % j][a] for j in range(k)]) for a in range(vector_size)])
-#    f.write(str(feature_matrix)+'\n')
-#f.close()
-#
-#
-#
-#
-#
+
 
 
 
